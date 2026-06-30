f6a40a32d723a40fa54cce2e251f63e6b4f31fd3
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2026-06-30 10:21:27 +0200

    feat(auth): create AuthInterceptor

---
+++ app/src/main/java/com/strimup/feature/auth/data/remote/AuthInterceptor.kt
@@ -0,0 +1,32 @@
+package com.strimup.feature.auth.data.remote
+
+import com.strimup.feature.auth.data.local.AuthPreferencesDataSource
+import javax.inject.Inject
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor @Inject constructor(
+    private val preferences: AuthPreferencesDataSource
+): Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+
+        val token = runBlocking {
+            preferences.authToken.firstOrNull()
+        }
+
+        val requestBuilder = request.newBuilder()
+            .header("X-Requested-With", "com.strimup.app")
+            .header("Accept", "application/json")
+
+        if(!token.isNullOrBlank()){
+            requestBuilder.header("Authorization", "Bearer $token")
+        }
+
+        val newRequest = requestBuilder.build()
+
+        return chain.proceed(newRequest)
+    }
+}
