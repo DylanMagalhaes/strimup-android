c0033340082055ffd1a13571746465fe2e83d181
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2026-07-17 17:04:43 +0200

    feat(profile): create UpdateProfileRequest

---
+++ app/src/main/java/com/strimup/feature/streamerprofile/data/request/UpdateProfileRequest.kt
@@ -0,0 +1,36 @@
+package com.strimup.feature.streamerprofile.data.request
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UpdateProfileRequest(
+    @SerialName("bio")
+    val bio: String?,
+    @SerialName("daily_status")
+    val dailyStatus: String?,
+    @SerialName("twitch_url")
+    val twitchUrl: String?,
+    @SerialName("youtube_url")
+    val youtubeUrl: String?,
+    @SerialName("tiktok_url")
+    val tiktokUrl: String?,
+    @SerialName("instagram_url")
+    val instagramUrl: String?,
+    @SerialName("kick_url")
+    val kickUrl: String?,
+    @SerialName("personality")
+    val personality: String?,
+    @SerialName("personality_secondary")
+    val personalitySecondary: String?,
+    @SerialName("stream_frequency")
+    val streamFrequency: String?,
+    @SerialName("average_viewers")
+    val averageViewers: Int?,
+    @SerialName("languages")
+    val languages: List<String>?,
+    @SerialName("tags")
+    val tags: List<Int> = emptyList(),
+    @SerialName("videos")
+    val videos: List<String> = emptyList()
+)
