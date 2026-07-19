01d5bd0fdd4410eae69ec4303d361beecc87a9a9
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2026-07-19 13:51:56 +0200

    feat(profile): inject usecases in the EditeProfileViewModele & create stateFlow

--- app/src/main/java/com/strimup/feature/streamerprofile/presentation/EditProfileViewModel.kt
+++ app/src/main/java/com/strimup/feature/streamerprofile/presentation/EditProfileViewModel.kt
@@ -1,4 +1,21 @@
 package com.strimup.feature.streamerprofile.presentation
 
-class EditProfileViewModel {
+import androidx.lifecycle.ViewModel
+import com.strimup.feature.streamerprofile.domain.usecase.GetStreamerUsecase
+import com.strimup.feature.streamerprofile.domain.usecase.UpdateProfileUsecase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+@HiltViewModel
+class EditProfileViewModel @Inject constructor(
+    private val getStreamer: GetStreamerUsecase,
+    private val updateProfile: UpdateProfileUsecase
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<EditProfileUiState>(EditProfileUiState.loading)
+    val uiState: StateFlow<EditProfileUiState> = _uiState.asStateFlow()
+
 }
