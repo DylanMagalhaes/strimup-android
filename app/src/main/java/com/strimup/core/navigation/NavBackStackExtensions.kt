dde59c9f458ba3fe380dd6aa6a634498cbcf0552
Dylan Magalhaes <magalhaes.dylan@gmail.com>
2026-09-11 13:43:10 +0200

    feat(navigation): fix  NavBackStackExtensions and implem in StrimupNavDisplay

--- app/src/main/java/com/strimup/core/navigation/NavBackStackExtensions.kt
+++ app/src/main/java/com/strimup/core/navigation/NavBackStackExtensions.kt
@@ -1,9 +1,8 @@
 package com.strimup.core.navigation
 
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.navigation3.runtime.NavKey
 
-fun <T : NavKey> SnapshotStateList<T>.navigateAsTab(destination: T) {
+fun <T : NavKey> MutableList<T>.navigateAsTab(destination: T) {
     if (lastOrNull() == destination) return
     remove(destination)
     add(destination)
